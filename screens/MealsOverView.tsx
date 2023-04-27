aa8d88e5457e75a59fcf2603c09b819d6bc459be
Hossam116 <hossam.baioumy@splintinvest.com>
2023-04-27 16:04:53 +0200

    adding react native navigation

---
+++ screens/MealsOverView.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MealsOverView = () => {
+  return (
+    <View>
+      <Text>MealsOverView</Text>
+    </View>
+  );
+};
+
+export default MealsOverView;
